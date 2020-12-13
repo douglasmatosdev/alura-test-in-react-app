7d0805300b35bae77eef35745b6de97a49a1f1a5
douglas.silva <douglas.silva@intelie.com.br>
2020-12-13 18:26:38 -0300

    Test events and user interators

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import App, { calcularNovoSaldo } from './App'
 
@@ -45,6 +45,27 @@ describe('Componente principal', () => {
             const novoSaldo = calcularNovoSaldo(valores, 150)
 
             expect(novoSaldo).toBe(250)
+        })
+        it('que é um saque, a transação dever ser realizada', () => {
+            const {
+                getByText,
+                getByTestId,
+                getByLabelText
+            } = render(<App />)
+
+            const saldo = getByText('R$ 1000')
+            const transacao = getByLabelText('Saque')
+            const valor = getByTestId('valor')
+            const botaoTransacao = getByText('Realizar operação')
+
+            expect(saldo.textContent).toBe('R$ 1000')
+
+            fireEvent.click(transacao, { target: { value: 'saque'} })
+            fireEvent.change(valor, { target: { value: 10 } })
+            fireEvent.click(botaoTransacao)
+
+
+            expect(saldo.textContent).toBe('R$ 990')
         })
     })
 })
